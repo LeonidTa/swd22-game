ca13d8331d2c71169c467c0768e62e47a96e922e
Leo <leonid.tahiraj@tutanota.com>
2022-05-13 17:09:59 +0200

    factory structure created

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/factories/DefaultFactory.java
@@ -0,0 +1,21 @@
+package at.compus02.swd.ss2022.game.gameobjects.factories;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class DefaultFactory implements AbstractFactory {
+
+    @Override
+    public void act(float delta) {
+
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+
+    }
+}
---
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/factories/GrasTile.java
@@ -0,0 +1,4 @@
+package at.compus02.swd.ss2022.game.gameobjects.factories;
+
+public class GrasTile implements Tile{
+}
